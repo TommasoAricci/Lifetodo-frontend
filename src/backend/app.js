bdcc93749f6d6a2a74747c152c829a23d44c1b23
tommasoaricci <ariccitommaso@hotmail.com>
2025-01-28 18:48:50 +0100

    proxy

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,38 +1,31 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 const app = express();
-const path = require("path");
-const todoRoutes = require("./routes/todoRoute");
-const thoughtRoutes = require("./routes/thoughtRoute");
-const userRoutes = require("./routes/userRoute");
-const songRoutes = require("./routes/songRoute");
+const path = require('path');
+const todoRoutes = require('./routes/todoRoute');
+const thoughtRoutes = require('./routes/thoughtRoute');
+const userRoutes = require('./routes/userRoute');
+const songRoutes = require('./routes/songRoute');
 
 app.use(express.json());
 
 const allowedOrigins = [
-  "http://localhost:3000", // Ambiente di sviluppo
-  "https://lifeist.netlify.app", // Sito Netlify in produzione
+  'http://localhost:3000', 
+  'http://localhost:4000', 
+  'https://lifeist.netlify.app/'
 ];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
-app.use(express.static(path.join(__dirname, "../../build")));
+app.use(express.static(path.join(__dirname, '../../build')));
 
 app.use("/api", todoRoutes, thoughtRoutes, userRoutes, songRoutes);
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../build/index.html"));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../build/index.html'));
 });
 
 module.exports = app;
