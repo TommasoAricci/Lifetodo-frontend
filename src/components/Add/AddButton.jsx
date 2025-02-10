640c037a9f71457c6efc115e1b3c17f3e7bab00f
tommasoaricci <ariccitommaso@hotmail.com>
2025-02-10 16:32:01 +0100

    war

--- src/components/Add/AddButton.jsx
+++ src/components/Add/AddButton.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useStore } from "../../store";
-import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPlus,
