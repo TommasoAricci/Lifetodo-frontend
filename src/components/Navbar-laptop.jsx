9ad0983b49463e3cb7c1dd69e02e96391463443c
tommasoaricci <ariccitommaso@hotmail.com>
2025-02-12 13:42:03 +0100

    faRight

--- src/components/Navbar-laptop.jsx
+++ src/components/Navbar-laptop.jsx
@@ -3,7 +3,7 @@ import "../style/navbar.scss";
 import image from "../images/copia.jpg";
 import { Link, useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faUser, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
   const location = useLocation();
